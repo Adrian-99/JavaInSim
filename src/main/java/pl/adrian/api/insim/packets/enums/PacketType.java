3b996cd5bf9b11ebb1f67d8fac910e6a7ee0c384
Adrian-99 <adrian.jurek.1109@gmail.com>
2023-08-07 20:24:44 +0200

    Added license + added license header to every source file + added LICENSE file

--- src/main/java/pl/adrian/api/insim/packets/enums/PacketType.java
+++ src/main/java/pl/adrian/api/insim/packets/enums/PacketType.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023, Adrian-99
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package pl.adrian.api.insim.packets.enums;
 
 import pl.adrian.api.insim.packets.*;
--- src/main/java/pl/adrian/internal/insim/packets/structures/ModSkinId.java
+++ src/main/java/pl/adrian/internal/insim/packets/structures/ModSkinId.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (c) 2023, Adrian-99
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 package pl.adrian.internal.insim.packets.structures;
 
 import pl.adrian.internal.insim.packets.structures.base.UnsignedInstructionStructure;
